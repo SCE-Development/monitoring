8dc72fcfd4b3641080587182cbc6f50bcabd796d
Yuri Tatishchev <itatishch@gmail.com>
2024-10-23 21:50:33 -0700

    dashboards: containers, basic node exporter

--- grafana/provisioning/dashboards-py/containers.dashboard.py
+++ grafana/provisioning/dashboards-py/containers.dashboard.py
@@ -1,61 +1,128 @@
 from grafanalib.core import (
     Dashboard, TimeSeries, GaugePanel,
     Target, GridPos,
-    OPS_FORMAT, Templating, Template, REFRESH_ON_TIME_RANGE_CHANGE
+    OPS_FORMAT, Templating, Template, REFRESH_ON_TIME_RANGE_CHANGE, Logs
 )
-from grafanalib.formatunits import BYTES_IEC
+from grafanalib.formatunits import BYTES_IEC, SECONDS, BYTES_SEC_IEC
 
-prom_datasource="PBFA97CFB590B2093"
+from common import LokiTarget, PrometheusTemplate
+
+prom_datasource = '${datasource}'
+# TODO: Loki (@weslayer)
+loki_datasource = 'loki'
 
 dashboard = Dashboard(
-    title="Containers",
-    description="Data for compose projects from default Prometheus datasource collected by Cadvisor",
+    title='Containers',
+    uid='containers',
+    description='Data for compose projects from default Prometheus datasource collected by Cadvisor',
     tags=[
-        'example'
+        'linux',
+        'docker',
     ],
     templating=Templating(list=[
-        # TODO: test how much of this is actually necessary
+        PrometheusTemplate,
         Template(
-            name="compose_project",
-            label="compose_project",
+            name='compose_project',
+            label='Compose Project',
             dataSource=prom_datasource,
             query='label_values({__name__=~"container.*"}, container_label_com_docker_compose_project)',
             includeAll=True,
             multi=True,
-            hide=0,
-            sort=1,
-            type="query",
             refresh=REFRESH_ON_TIME_RANGE_CHANGE,
         ),
         Template(
-            name="container_name",
-            label="container_name",
+            name='container_name',
+            label='Container',
             dataSource=prom_datasource,
             query='label_values({__name__=~"container.*", container_label_com_docker_compose_project=~"$compose_project"}, name)',
             includeAll=True,
             multi=True,
-            hide=0,
-            sort=1,
-            type="query",
             refresh=REFRESH_ON_TIME_RANGE_CHANGE,
-
+        ),
+        Template(
+            name='logs_query',
+            label='Log Search',
+            query='',
+            type='textbox',
         ),
     ]),
-    timezone="browser",
+    timezone='browser',
     panels=[
         TimeSeries(
-            title="Container Memory Usage",
+            title='Container Memory Usage',
+            unit=BYTES_IEC,
+            gridPos=GridPos(h=8, w=12, x=0, y=0),
+            lineWidth=2,
+            fillOpacity=10,
+            showPoints='never',
+            stacking={'mode': 'normal'},
+            tooltipMode='all',
+            tooltipSort='desc',
             targets=[
                 Target(
                     datasource=prom_datasource,
                     expr='max by (name) (container_memory_usage_bytes{name=~"$container_name", container_label_com_docker_compose_project=~"$compose_project"})',
-                    legendFormat="{{ name }}",
+                    legendFormat='{{ name }}',
                     refId='A',
                 ),
             ],
-            unit=BYTES_IEC,
-            gridPos=GridPos(h=8, w=16, x=0, y=0),
+        ),
+        TimeSeries(
+            title='Container CPU Usage',
+            unit=SECONDS,
+            gridPos=GridPos(h=8, w=12, x=12, y=0),
+            lineWidth=2,
+            fillOpacity=10,
+            showPoints='never',
+            targets=[
+                Target(
+                    datasource=prom_datasource,
+                    expr='max by (name) (rate(container_cpu_usage_seconds_total{name=~"$container_name", container_label_com_docker_compose_project=~"$compose_project"}[$__rate_interval]))',
+                    legendFormat='{{ name }}',
+                    refId='A',
+                ),
+            ],
+        ),
+        TimeSeries(
+            title='Container Network Traffic',
+            unit=BYTES_SEC_IEC,
+            gridPos=GridPos(h=8, w=12, x=0, y=8),
+            lineWidth=2,
+            fillOpacity=10,
+            showPoints='never',
+            tooltipMode='all',
+            tooltipSort='desc',
+            targets=[
+                Target(
+                    datasource=prom_datasource,
+                    expr='max by (name) (rate(container_network_receive_bytes_total{name=~"$container_name", container_label_com_docker_compose_project=~"$compose_project"}[$__rate_interval]))',
+                    legendFormat="rx {{ name }}",
+                    refId='A',
+                ),
+                Target(
+                    datasource=prom_datasource,
+                    expr='-max by (name) (rate(container_network_transmit_bytes_total{name=~"$container_name", container_label_com_docker_compose_project=~"$compose_project"}[$__rate_interval]))',
+                    legendFormat="tx {{ name }}",
+                    refId='B',
+                ),
+            ],
+        ),
+        Logs(
+            title='',
+            gridPos=GridPos(h=8, w=12, x=12, y=8),
+            showLabels=True,
+            showCommonLabels=True,
+            wrapLogMessages=True,
+            prettifyLogMessage=True,
+            dedupStrategy='numbers',
+            targets=[
+                LokiTarget(
+                    loki_datasource=loki_datasource,
+                    expr='{compose_project=~"$compose_project", container_name=~"$container_name"} |= `$logs_query`',
+                    legendFormat='{{ container_name }}',
+                    refId='A',
+                ),
+            ],
         ),
     ],
 ).auto_panel_ids()
-
