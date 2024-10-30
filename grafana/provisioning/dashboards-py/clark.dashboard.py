b8829feb8e1d08d5895002fb0ec36d1a530113e6
weslayer <wesleywong1245@yahoo.com>
2024-10-30 15:45:37 -0700

    add clark dashboard

---
+++ grafana/provisioning/dashboards-py/clark.dashboard.py
@@ -0,0 +1,65 @@
+from grafanalib.core import Dashboard, Templating, Template, TimeSeries, Target, GridPos
+from grafanalib.formatunits import PERCENT_UNIT, SECONDS, NUMBER_FORMAT, TRUE_FALSE
+
+from common import PrometheusTemplate
+
+dashboard = Dashboard(
+    title='Clark',
+    uid='clark',
+    description='sce club website',
+    timezone='browser',
+    templating=Templating(list=[
+        # Datasource
+        PrometheusTemplate,
+    ]),
+    panels=[
+        TimeSeries(
+            title='Clark main-endpoint hits',
+            unit=NUMBER_FORMAT,
+            gridPos=GridPos(h=8, w=12, x=0, y=0),
+            lineWidth=2,
+            stacking={'group': 'A','mode': 'none'},
+            tooltipMode='all',
+            tooltipSort='desc',
+            targets=[
+                Target(
+                    datasource='${datasource}',
+                    expr='endpoint_hits{route!="/metrics"}',
+                    refId='A',
+                ),
+            ],
+        ),
+        TimeSeries(
+            title='Clark main-endpoint hits',
+            unit=NUMBER_FORMAT,
+            gridPos=GridPos(h=8, w=12, x=0, y=0),
+            lineWidth=2,
+            stacking={'group': 'A','mode': 'none'},
+            tooltipMode='all',
+            tooltipSort='desc',
+            targets=[
+                Target(
+                    datasource='${datasource}',
+                    expr='endpoint_hits{route=~"/(sendPasswordReset|validatePasswordReset|resetPassword)"}',
+                    refId='A',
+                ),
+            ],
+        ),
+        TimeSeries(
+            title='Clark - messages',
+            unit=NUMBER_FORMAT,
+            gridPos=GridPos(h=8, w=12, x=0, y=0),
+            lineWidth=2,
+            stacking={'group': 'A','mode': 'none'},
+            tooltipMode='all',
+            tooltipSort='desc',
+            targets=[
+                Target(
+                    datasource='${datasource}',
+                    expr='endpoint_hits{route=~"/(send|listen|getLatestMessage)"}',
+                    refId='A',
+                ),
+            ],
+        ),
+    ],
+).auto_panel_ids()
