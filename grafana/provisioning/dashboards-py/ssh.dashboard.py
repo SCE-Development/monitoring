9fa8b25aa2eaabc65bc6489d6fb3f6ef28c72fd3
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2024-12-07 19:38:25 -0800

    Resolve invalid dashboard UID alert in ssh dashboard (#14)
    * Resolve invalid dashboard UID alert in ssh dashboard
    
    * remove overrides
    
    * remove default dashboard home path
    
    * make ssh py change just the uid line

--- grafana/provisioning/dashboards-py/ssh.dashboard.py
+++ grafana/provisioning/dashboards-py/ssh.dashboard.py
@@ -39,7 +39,7 @@ uptime_thresholds=[
 
 dashboard = Dashboard(
     title='SSH Tunnel Health',
-    uid='SSHTunnelHealth',
+    uid='ssh',
     description='Health of SSH Tunnel',
     timezone='browser',
     panels=[
