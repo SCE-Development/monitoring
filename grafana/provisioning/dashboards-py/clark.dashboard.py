3c0388c5f52c814010987cfbc9f2d542dc3d8e99
evan <evanuxd@gmail.com>
2025-03-15 11:04:43 -0700

    make 2 rows

--- grafana/provisioning/dashboards-py/clark.dashboard.py
+++ grafana/provisioning/dashboards-py/clark.dashboard.py
@@ -14,7 +14,7 @@ from common import PROMETHEUS_DATASOURCE_NAME
 
 def get_office_card_row():
     return Row(
-        title="Example Row",
+        title="Office Access Card",
         panels=[
             TimeSeries(
                 title="Office Access Card - 200 Responses",
@@ -23,11 +23,13 @@ def get_office_card_row():
                         datasource=PROMETHEUS_DATASOURCE_NAME,
                         expr='sum(endpoint_hits{route=~"/api/OfficeAccessCard/verify.*", statusCode="200"})',
                         legendFormat="total",
+                        refId="A",
                     ),
                     Target(
                         datasource=PROMETHEUS_DATASOURCE_NAME,
                         expr='sum(rate(endpoint_hits{route=~"/api/OfficeAccessCard/verify.*", statusCode="200"}[1h])) * 3600',
                         legendFormat="dY/dt [hourly]",
+                        refId="B",
                     ),
                 ],
             ),
@@ -38,11 +40,89 @@ def get_office_card_row():
                         datasource=PROMETHEUS_DATASOURCE_NAME,
                         expr='sum(endpoint_hits{route=~"/api/OfficeAccessCard/verify.*", statusCode!="200"}) by (statusCode)',
                         legendFormat="{{statusCode}} total",
+                        refId="A",
                     ),
                     Target(
                         datasource=PROMETHEUS_DATASOURCE_NAME,
                         expr='sum(rate(endpoint_hits{route=~"/api/OfficeAccessCard/verify.*", statusCode!="200"}[1h])) by (statusCode) * 3600',
                         legendFormat="{{statusCode}} dY/dt [hourly]",
+                        refId="B",
+                    ),
+                ],
+            ),
+        ],
+    )
+
+
+def get_other_row():
+    return Row(
+        title="Example Row",
+        panels=[
+            TimeSeries(
+                title="All main-endpoints traffic",
+                unit=NUMBER_FORMAT,
+                gridPos=GridPos(h=8, w=12, x=0, y=0),
+                lineWidth=2,
+                stacking={"group": "A", "mode": "none"},
+                tooltipMode="all",
+                tooltipSort="desc",
+                targets=[
+                    Target(
+                        datasource=PROMETHEUS_DATASOURCE_NAME,
+                        expr='endpoint_hits{route!="/metrics"}',
+                        legendFormat="{{route}} {{method}} {{statusCode}}",
+                        refId="A",
+                    ),
+                ],
+            ),
+            TimeSeries(
+                title="Account access",
+                unit=NUMBER_FORMAT,
+                gridPos=GridPos(h=8, w=12, x=12, y=0),
+                lineWidth=2,
+                stacking={"group": "A", "mode": "none"},
+                tooltipMode="all",
+                tooltipSort="desc",
+                targets=[
+                    Target(
+                        datasource=PROMETHEUS_DATASOURCE_NAME,
+                        expr='endpoint_hits{route=~"/api/Auth.*"}',
+                        legendFormat="{{route}} {{method}} {{statusCode}}",
+                        refId="A",
+                    ),
+                ],
+            ),
+            TimeSeries(
+                title="Office Access Card",
+                unit=NUMBER_FORMAT,
+                gridPos=GridPos(h=8, w=12, x=0, y=8),
+                lineWidth=2,
+                stacking={"group": "A", "mode": "none"},
+                tooltipMode="all",
+                tooltipSort="desc",
+                targets=[
+                    Target(
+                        datasource=PROMETHEUS_DATASOURCE_NAME,
+                        expr='endpoint_hits{route="/api/OfficeAccessCard/verify"}',
+                        legendFormat="{{route}} {{method}} {{statusCode}}",
+                        refId="A",
+                    ),
+                ],
+            ),
+            TimeSeries(
+                title="Messaging",
+                unit=NUMBER_FORMAT,
+                gridPos=GridPos(h=8, w=12, x=12, y=8),
+                lineWidth=2,
+                stacking={"group": "A", "mode": "none"},
+                tooltipMode="all",
+                tooltipSort="desc",
+                targets=[
+                    Target(
+                        datasource=PROMETHEUS_DATASOURCE_NAME,
+                        expr='endpoint_hits{route=~"/api/messages.*"}',
+                        legendFormat="{{route}} {{method}} {{statusCode}}",
+                        refId="A",
                     ),
                 ],
             ),
@@ -55,75 +135,5 @@ dashboard = Dashboard(
     uid="clark",
     description="sce club website",
     timezone="browser",
-    rows=[get_office_card_row()],
-    panels=[
-        TimeSeries(
-            title="All main-endpoints traffic",
-            unit=NUMBER_FORMAT,
-            gridPos=GridPos(h=8, w=12, x=0, y=0),
-            lineWidth=2,
-            stacking={"group": "A", "mode": "none"},
-            tooltipMode="all",
-            tooltipSort="desc",
-            targets=[
-                Target(
-                    datasource=PROMETHEUS_DATASOURCE_NAME,
-                    expr='endpoint_hits{route!="/metrics"}',
-                    legendFormat="{{route}} {{method}} {{statusCode}}",
-                    refId="A",
-                ),
-            ],
-        ),
-        TimeSeries(
-            title="Account access",
-            unit=NUMBER_FORMAT,
-            gridPos=GridPos(h=8, w=12, x=12, y=0),
-            lineWidth=2,
-            stacking={"group": "A", "mode": "none"},
-            tooltipMode="all",
-            tooltipSort="desc",
-            targets=[
-                Target(
-                    datasource=PROMETHEUS_DATASOURCE_NAME,
-                    expr='endpoint_hits{route=~"/api/Auth.*"}',
-                    legendFormat="{{route}} {{method}} {{statusCode}}",
-                    refId="A",
-                ),
-            ],
-        ),
-        TimeSeries(
-            title="Office Access Card",
-            unit=NUMBER_FORMAT,
-            gridPos=GridPos(h=8, w=12, x=0, y=8),
-            lineWidth=2,
-            stacking={"group": "A", "mode": "none"},
-            tooltipMode="all",
-            tooltipSort="desc",
-            targets=[
-                Target(
-                    datasource=PROMETHEUS_DATASOURCE_NAME,
-                    expr='endpoint_hits{route="/api/OfficeAccessCard/verify"}',
-                    legendFormat="{{route}} {{method}} {{statusCode}}",
-                    refId="A",
-                ),
-            ],
-        ),
-        TimeSeries(
-            title="Messaging",
-            unit=NUMBER_FORMAT,
-            gridPos=GridPos(h=8, w=12, x=12, y=8),
-            lineWidth=2,
-            stacking={"group": "A", "mode": "none"},
-            tooltipMode="all",
-            tooltipSort="desc",
-            targets=[
-                Target(
-                    datasource=PROMETHEUS_DATASOURCE_NAME,
-                    expr='endpoint_hits{route=~"/api/messages.*"}',
-                    legendFormat="{{route}} {{method}} {{statusCode}}",
-                    refId="A",
-                ),
-            ],
-        ),
-    ],
+    rows=[get_office_card_row(), get_other_row],
 ).auto_panel_ids()
