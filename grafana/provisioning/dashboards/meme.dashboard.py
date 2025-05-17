e78b88499c58eb2a3efeaa12a1dec5c6a9649781
evan <evanuxd@gmail.com>
2025-05-16 21:08:49 -0700

    second expression in 1 panel

--- grafana/provisioning/dashboards/meme.dashboard.py
+++ grafana/provisioning/dashboards/meme.dashboard.py
@@ -3,7 +3,10 @@ import wrapper
 asdf = wrapper.SceGrafanalibWrapper("hello world", "testing wrapper class")
 
 asdf.DefineRow("first row")
-asdf.AddPanelToRow("bing bong", [wrapper.ExpressionAndLegendPair(expression="time()")])
+asdf.AddPanelToRow("bing bong", [
+    wrapper.ExpressionAndLegendPair(expression="time()"),
+    wrapper.ExpressionAndLegendPair(expression="time()"),
+])
 asdf.AddPanelToRow("bing bong1", [wrapper.ExpressionAndLegendPair(expression="time()")])
 asdf.AddPanelToRow("bing bong2", [wrapper.ExpressionAndLegendPair(expression="time()")])
 asdf.AddPanelToRow("bing bong3", [wrapper.ExpressionAndLegendPair(expression="time()")])
