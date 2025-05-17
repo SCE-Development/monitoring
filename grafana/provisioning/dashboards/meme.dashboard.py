91a90d41d3ef55e3444ce5886b2b3cb098a147cf
evan <evanuxd@gmail.com>
2025-05-16 20:25:00 -0700

    here comes the spam

--- grafana/provisioning/dashboards/meme.dashboard.py
+++ grafana/provisioning/dashboards/meme.dashboard.py
@@ -1,9 +1,9 @@
 import wrapper
 
-dashboard = wrapper.SceGrafanalibWrapper("hello world", "testing wrapper class")
+asdf = wrapper.SceGrafanalibWrapper("hello world", "testing wrapper class")
 
-dashboard.DefineRow("silly row")
-dashboard.AddPanelToRow("bing bong", [wrapper.ExpressionAndLegendPair(expression="scalar(42)")])
-dashboard.AddPanel("outside gang", [wrapper.ExpressionAndLegendPair(expression='scalar(42)')])
+asdf.DefineRow("silly row")
+asdf.AddPanelToRow("bing bong", [wrapper.ExpressionAndLegendPair(expression="scalar(42)")])
+asdf.AddPanel("outside gang", [wrapper.ExpressionAndLegendPair(expression='scalar(42)')])
 
-dashboard.Render()
+asdf.Render()
