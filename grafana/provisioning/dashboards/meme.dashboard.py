c24cf3859e70f8fc4e93ee353f7d2cde2718c3fb
evan <evanuxd@gmail.com>
2025-05-16 20:24:20 -0700

    default None value

--- grafana/provisioning/dashboards/meme.dashboard.py
+++ grafana/provisioning/dashboards/meme.dashboard.py
@@ -5,3 +5,5 @@ dashboard = wrapper.SceGrafanalibWrapper("hello world", "testing wrapper class")
 dashboard.DefineRow("silly row")
 dashboard.AddPanelToRow("bing bong", [wrapper.ExpressionAndLegendPair(expression="scalar(42)")])
 dashboard.AddPanel("outside gang", [wrapper.ExpressionAndLegendPair(expression='scalar(42)')])
+
+dashboard.Render()
