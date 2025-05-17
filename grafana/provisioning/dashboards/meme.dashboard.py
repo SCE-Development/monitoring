b4a2f1ba1fa6b493c3a197cbf144fb2dead445ba
evan <evanuxd@gmail.com>
2025-05-16 21:09:52 -0700

    test legends

--- grafana/provisioning/dashboards/meme.dashboard.py
+++ grafana/provisioning/dashboards/meme.dashboard.py
@@ -4,8 +4,8 @@ asdf = wrapper.SceGrafanalibWrapper("hello world", "testing wrapper class")
 
 asdf.DefineRow("first row")
 asdf.AddPanelToRow("bing bong", [
-    wrapper.ExpressionAndLegendPair(expression="time()"),
-    wrapper.ExpressionAndLegendPair(expression="time()"),
+    wrapper.ExpressionAndLegendPair(expression="time()", legend='my first line'),
+    wrapper.ExpressionAndLegendPair(expression="time()", legend='second line'),
 ])
 asdf.AddPanelToRow("bing bong1", [wrapper.ExpressionAndLegendPair(expression="time()")])
 asdf.AddPanelToRow("bing bong2", [wrapper.ExpressionAndLegendPair(expression="time()")])
