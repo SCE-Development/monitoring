b83a7a3c5159be3a1be889e5da9e76831514b938
evan <evanuxd@gmail.com>
2025-05-16 20:28:00 -0700

    use time

--- grafana/provisioning/dashboards/meme.dashboard.py
+++ grafana/provisioning/dashboards/meme.dashboard.py
@@ -3,7 +3,7 @@ import wrapper
 asdf = wrapper.SceGrafanalibWrapper("hello world", "testing wrapper class")
 
 asdf.DefineRow("silly row")
-asdf.AddPanelToRow("bing bong", [wrapper.ExpressionAndLegendPair(expression="scalar(42)")])
-asdf.AddPanel("outside gang", [wrapper.ExpressionAndLegendPair(expression='scalar(42)')])
+asdf.AddPanelToRow("bing bong", [wrapper.ExpressionAndLegendPair(expression="time()")])
+asdf.AddPanel("outside gang", [wrapper.ExpressionAndLegendPair(expression='time()')])
 
 dashboard = asdf.Render()
