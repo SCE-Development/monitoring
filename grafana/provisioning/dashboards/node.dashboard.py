a997c0bfa4fd8dfbdb40e13a6d82bd616a8c8f36
andresaclan <aclanandres@gmail.com>
2025-06-06 15:12:50 -0700

    define timeseries panels from list

--- grafana/provisioning/dashboards/node.dashboard.py
+++ grafana/provisioning/dashboards/node.dashboard.py
@@ -1,8 +1,25 @@
 from grafanalib.core import Dashboard, Templating, Template, TimeSeries, GridPos, Target, Row, GaugePanel
 from grafanalib.formatunits import BYTES_IEC, PERCENT_UNIT, BYTES_SEC_IEC
 
-from common import PROMETHEUS_DATASOURCE_NAME
-from node_consts import CPU_BASIC_COLORS, MEMORY_BASIC_COLORS, GAUGE_CONFIGS 
+PROMETHEUS_DATASOURCE_NAME = 'Prometheus'
+from node_consts import CPU_BASIC_COLORS, MEMORY_BASIC_COLORS, GAUGE_CONFIGS, TIMESERIES_CONFIGS 
+
+def create_timeseries_panel(config):
+    """Create a TimeSeries panel from a configuration dictionary."""
+    return TimeSeries(
+        title=config['title'],
+        description=config.get('description', ''),
+        unit=config['unit'],
+        gridPos=GridPos(h=8, w=12, x=config['x_pos'], y=config.get('y_pos', 0)),
+        lineWidth=config.get('lineWidth', 1),
+        fillOpacity=config.get('fillOpacity', 30),
+        showPoints='never',
+        stacking=config.get('stacking', {'mode': 'none'}),
+        tooltipMode='all',
+        tooltipSort='desc',
+        targets=[Target(**target) for target in config['targets']],
+        extraJson=config.get('extraJson', {})
+    )
 
 def create_gauge_panel(config):
     """Create a GaugePanel from a configuration dictionary."""
@@ -61,136 +78,7 @@ dashboard = Dashboard(
         ),
         Row(
             title='Basic CPU / Mem / Net / Disk',
-            panels=[
-                # CPU Basic
-                TimeSeries(
-                    title='CPU Basic',
-                    description='Basic CPU usage info',
-                    unit=PERCENT_UNIT,
-                    gridPos=GridPos(h=8, w=12, x=0, y=0),
-                    lineWidth=1,
-                    fillOpacity=30,
-                    showPoints='never',
-                    stacking={'mode': 'percent', 'group': 'A'},
-                    tooltipMode='all',
-                    tooltipSort='desc',
-                    targets=[
-                        Target(
-                            datasource=PROMETHEUS_DATASOURCE_NAME,
-                            expr='sum(irate(node_cpu_seconds_total{instance="$instance",job="$job", mode="system"}[$__rate_interval])) / scalar(count(count(node_cpu_seconds_total{instance="$instance",job="$job"}) by (cpu)))',
-                            legendFormat='Busy System',
-                            refId='A',
-                        ),
-                        Target(
-                            datasource=PROMETHEUS_DATASOURCE_NAME,
-                            expr='sum(irate(node_cpu_seconds_total{instance="$instance",job="$job", mode="user"}[$__rate_interval])) / scalar(count(count(node_cpu_seconds_total{instance="$instance",job="$job"}) by (cpu)))',
-                            legendFormat='Busy User',
-                            refId='B',
-                        ),
-                        Target(
-                            datasource=PROMETHEUS_DATASOURCE_NAME,
-                            expr='sum(irate(node_cpu_seconds_total{instance="$instance",job="$job", mode="iowait"}[$__rate_interval])) / scalar(count(count(node_cpu_seconds_total{instance="$instance",job="$job"}) by (cpu)))',
-                            legendFormat='Busy Iowait',
-                            refId='C',
-                        ),
-                        Target(
-                            datasource=PROMETHEUS_DATASOURCE_NAME,
-                            expr='sum(irate(node_cpu_seconds_total{instance="$instance",job="$job", mode=~".*irq"}[$__rate_interval])) / scalar(count(count(node_cpu_seconds_total{instance="$instance",job="$job"}) by (cpu)))',
-                            legendFormat='Busy IRQs',
-                            refId='D',
-                        ),
-                        Target(
-                            datasource=PROMETHEUS_DATASOURCE_NAME,
-                            expr='sum(irate(node_cpu_seconds_total{instance="$instance",job="$job",  mode!="idle",mode!="user",mode!="system",mode!="iowait",mode!="irq",mode!="softirq"}[$__rate_interval])) / scalar(count(count(node_cpu_seconds_total{instance="$instance",job="$job"}) by (cpu)))',
-                            legendFormat='Busy Other',
-                            refId='E',
-                        ),
-                        Target(
-                            datasource=PROMETHEUS_DATASOURCE_NAME,
-                            expr='sum(irate(node_cpu_seconds_total{instance="$instance",job="$job", mode="idle"}[$__rate_interval])) / scalar(count(count(node_cpu_seconds_total{instance="$instance",job="$job"}) by (cpu)))',
-                            legendFormat='Idle',
-                            refId='F',
-                        ),
-                    ],
-                    # Extra JSON for the colors
-                    extraJson=CPU_BASIC_COLORS,
-                ),
-                # Memory Basic
-                TimeSeries(
-                    title='Memory Basic',
-                    description='Basic memory usage',
-                    unit=BYTES_IEC,
-                    gridPos=GridPos(h=8, w=12, x=12, y=0),
-                    lineWidth=1,
-                    fillOpacity=30,
-                    showPoints='never',
-                    stacking={'mode': 'normal', 'group': 'A'},
-                    tooltipMode='all',
-                    tooltipSort='desc',
-                    targets=[
-                        Target(
-                            datasource=PROMETHEUS_DATASOURCE_NAME,
-                            expr='node_memory_MemTotal_bytes{instance="$instance",job="$job"}',
-                            format='time_series',
-                            legendFormat='RAM Total',
-                            refId='A',
-                        ),
-                        Target(
-                            datasource=PROMETHEUS_DATASOURCE_NAME,
-                            expr='node_memory_MemTotal_bytes{instance="$instance",job="$job"} - node_memory_MemFree_bytes{instance="$instance",job="$job"} - (node_memory_Cached_bytes{instance="$instance",job="$job"} + node_memory_Buffers_bytes{instance="$instance",job="$job"} + node_memory_SReclaimable_bytes{instance="$instance",job="$job"})',
-                            format='time_series',
-                            legendFormat='RAM Used',
-                            refId='B',
-                        ),
-                        Target(
-                            datasource=PROMETHEUS_DATASOURCE_NAME,
-                            expr='node_memory_Cached_bytes{instance="$instance",job="$job"} + node_memory_Buffers_bytes{instance="$instance",job="$job"} + node_memory_SReclaimable_bytes{instance="$instance",job="$job"}',
-                            legendFormat='RAM Cache + Buffer',
-                            refId='C',
-                        ),
-                        Target(
-                            datasource=PROMETHEUS_DATASOURCE_NAME,
-                            expr='node_memory_MemFree_bytes{instance="$instance",job="$job"}',
-                            legendFormat='RAM Free',
-                            refId='D',
-                        ),
-                        Target(
-                            datasource=PROMETHEUS_DATASOURCE_NAME,
-                            expr='(node_memory_SwapTotal_bytes{instance="$instance",job="$job"} - node_memory_SwapFree_bytes{instance="$instance",job="$job"})',
-                            legendFormat='SWAP Used',
-                            refId='E',
-                        ),
-                    ],
-                    # Extra JSON for the colors
-                    extraJson=MEMORY_BASIC_COLORS,
-                    ),
-                # Network Basic
-                TimeSeries(
-                    title='Network Traffic',
-                    unit=BYTES_SEC_IEC,
-                    gridPos=GridPos(h=8, w=12, x=0, y=8),
-                    lineWidth=2,
-                    fillOpacity=10,
-                    showPoints='never',
-                    tooltipMode='all',
-                    tooltipSort='desc',
-                    targets=[
-                        Target(
-                            datasource=PROMETHEUS_DATASOURCE_NAME,
-                            expr='rate(node_network_receive_bytes_total{instance="$instance",job="$job",device!="lo"}[$__rate_interval])',
-                            legendFormat="rx {{ device }}",
-                            refId='A',
-                        ),
-                        Target(
-                            datasource=PROMETHEUS_DATASOURCE_NAME,
-                            expr='-rate(node_network_transmit_bytes_total{instance="$instance",job="$job",device!="lo"}[$__rate_interval])',
-                            legendFormat="tx {{ device }}",
-                            refId='B',
-                        ),
-                    ],
-                ),
-                # TODO: Disk Basic
-            ],
+            panels=[create_timeseries_panel(config) for config in TIMESERIES_CONFIGS],
         )
     ]
 ).auto_panel_ids()
--- grafana/provisioning/dashboards/node_consts.py
+++ grafana/provisioning/dashboards/node_consts.py
@@ -1,3 +1,6 @@
+from grafanalib.formatunits import BYTES_IEC, PERCENT_UNIT, BYTES_SEC_IEC
+from common import PROMETHEUS_DATASOURCE_NAME
+
 # TODO: Question life decisions (I'm not sure if this is good)
 
 CPU_BASIC_COLORS = {
@@ -527,4 +530,118 @@ GAUGE_CONFIGS = [
   / node_filesystem_size_bytes{instance="$instance", job="$job", mountpoint="/", fstype!="rootfs"}
 ) * 100'''
     }
+]
+
+# TimeSeries panel configurations
+TIMESERIES_CONFIGS = [
+    {
+        'title': 'CPU Basic',
+        'description': 'Basic CPU usage info',
+        'unit': PERCENT_UNIT,
+        'x_pos': 0,
+        'stacking': {'mode': 'percent', 'group': 'A'},
+        'targets': [
+            {
+                'datasource': PROMETHEUS_DATASOURCE_NAME,
+                'expr': 'sum(irate(node_cpu_seconds_total{instance="$instance",job="$job", mode="system"}[$__rate_interval])) / scalar(count(count(node_cpu_seconds_total{instance="$instance",job="$job"}) by (cpu)))',
+                'legendFormat': 'Busy System',
+                'refId': 'A',
+            },
+            {
+                'datasource': PROMETHEUS_DATASOURCE_NAME,
+                'expr': 'sum(irate(node_cpu_seconds_total{instance="$instance",job="$job", mode="user"}[$__rate_interval])) / scalar(count(count(node_cpu_seconds_total{instance="$instance",job="$job"}) by (cpu)))',
+                'legendFormat': 'Busy User',
+                'refId': 'B',
+            },
+            {
+                'datasource': PROMETHEUS_DATASOURCE_NAME,
+                'expr': 'sum(irate(node_cpu_seconds_total{instance="$instance",job="$job", mode="iowait"}[$__rate_interval])) / scalar(count(count(node_cpu_seconds_total{instance="$instance",job="$job"}) by (cpu)))',
+                'legendFormat': 'Busy Iowait',
+                'refId': 'C',
+            },
+            {
+                'datasource': PROMETHEUS_DATASOURCE_NAME,
+                'expr': 'sum(irate(node_cpu_seconds_total{instance="$instance",job="$job", mode=~".*irq"}[$__rate_interval])) / scalar(count(count(node_cpu_seconds_total{instance="$instance",job="$job"}) by (cpu)))',
+                'legendFormat': 'Busy IRQs',
+                'refId': 'D',
+            },
+            {
+                'datasource': PROMETHEUS_DATASOURCE_NAME,
+                'expr': 'sum(irate(node_cpu_seconds_total{instance="$instance",job="$job",  mode!="idle",mode!="user",mode!="system",mode!="iowait",mode!="irq",mode!="softirq"}[$__rate_interval])) / scalar(count(count(node_cpu_seconds_total{instance="$instance",job="$job"}) by (cpu)))',
+                'legendFormat': 'Busy Other',
+                'refId': 'E',
+            },
+            {
+                'datasource': PROMETHEUS_DATASOURCE_NAME,
+                'expr': 'sum(irate(node_cpu_seconds_total{instance="$instance",job="$job", mode="idle"}[$__rate_interval])) / scalar(count(count(node_cpu_seconds_total{instance="$instance",job="$job"}) by (cpu)))',
+                'legendFormat': 'Idle',
+                'refId': 'F',
+            },
+        ],
+        'extraJson': CPU_BASIC_COLORS,
+    },
+    {
+        'title': 'Memory Basic',
+        'description': 'Basic memory usage',
+        'unit': BYTES_IEC,
+        'x_pos': 12,
+        'stacking': {'mode': 'normal', 'group': 'A'},
+        'targets': [
+            {
+                'datasource': PROMETHEUS_DATASOURCE_NAME,
+                'expr': 'node_memory_MemTotal_bytes{instance="$instance",job="$job"}',
+                'format': 'time_series',
+                'legendFormat': 'RAM Total',
+                'refId': 'A',
+            },
+            {
+                'datasource': PROMETHEUS_DATASOURCE_NAME,
+                'expr': 'node_memory_MemTotal_bytes{instance="$instance",job="$job"} - node_memory_MemFree_bytes{instance="$instance",job="$job"} - (node_memory_Cached_bytes{instance="$instance",job="$job"} + node_memory_Buffers_bytes{instance="$instance",job="$job"} + node_memory_SReclaimable_bytes{instance="$instance",job="$job"})',
+                'format': 'time_series',
+                'legendFormat': 'RAM Used',
+                'refId': 'B',
+            },
+            {
+                'datasource': PROMETHEUS_DATASOURCE_NAME,
+                'expr': 'node_memory_Cached_bytes{instance="$instance",job="$job"} + node_memory_Buffers_bytes{instance="$instance",job="$job"} + node_memory_SReclaimable_bytes{instance="$instance",job="$job"}',
+                'legendFormat': 'RAM Cache + Buffer',
+                'refId': 'C',
+            },
+            {
+                'datasource': PROMETHEUS_DATASOURCE_NAME,
+                'expr': 'node_memory_MemFree_bytes{instance="$instance",job="$job"}',
+                'legendFormat': 'RAM Free',
+                'refId': 'D',
+            },
+            {
+                'datasource': PROMETHEUS_DATASOURCE_NAME,
+                'expr': '(node_memory_SwapTotal_bytes{instance="$instance",job="$job"} - node_memory_SwapFree_bytes{instance="$instance",job="$job"})',
+                'legendFormat': 'SWAP Used',
+                'refId': 'E',
+            },
+        ],
+        'extraJson': MEMORY_BASIC_COLORS,
+    },
+    {
+        'title': 'Network Traffic',
+        'unit': BYTES_SEC_IEC,
+        'x_pos': 0,
+        'y_pos': 8,
+        'lineWidth': 2,
+        'fillOpacity': 10,
+        'targets': [
+            {
+                'datasource': PROMETHEUS_DATASOURCE_NAME,
+                'expr': 'rate(node_network_receive_bytes_total{instance="$instance",job="$job",device!="lo"}[$__rate_interval])',
+                'legendFormat': 'rx {{ device }}',
+                'refId': 'A',
+            },
+            {
+                'datasource': PROMETHEUS_DATASOURCE_NAME,
+                'expr': '-rate(node_network_transmit_bytes_total{instance="$instance",job="$job",device!="lo"}[$__rate_interval])',
+                'legendFormat': 'tx {{ device }}',
+                'refId': 'B',
+            },
+        ],
+    },
 ]
