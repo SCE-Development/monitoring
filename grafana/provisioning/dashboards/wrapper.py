4dba3d611469b160dc9c3b26564a91170b17ab8b
tofu <isabelleluu0w0@gmail.com>
2025-06-14 15:37:03 -0700

    format/beautify wrapper file

--- grafana/provisioning/dashboards/wrapper.py
+++ grafana/provisioning/dashboards/wrapper.py
@@ -1,14 +1,13 @@
 from dataclasses import dataclass
 from typing import Optional, Final
 
-from grafanalib.core import (
-    Row, Dashboard, Target, TimeSeries, GridPos
-)
+from grafanalib.core import Row, Dashboard, Target, TimeSeries, GridPos
 
 from common import PROMETHEUS_DATASOURCE_NAME
 
+
 class RefIdGenerator:
-    STARTING_CHAR_INTEGER = ord('A')
+    STARTING_CHAR_INTEGER = ord("A")
 
     def __init__(self):
         self.offset = 0
@@ -18,10 +17,12 @@ class RefIdGenerator:
         self.offset += 1
         return result
 
+
 @dataclass
 class ExpressionAndLegendPair:
     expression: str
     legend: Optional[str] = None
+
 
 class SceGrafanalibWrapper:
     MAX_WIDTH: Final[int] = 24
@@ -42,16 +43,32 @@ class SceGrafanalibWrapper:
             query_text = query.expression
             query_label = query.legend
             refId = iterator.next()
-            targets.append(Target(expr=query_text, legendFormat=query_label, refId=refId, datasource=PROMETHEUS_DATASOURCE_NAME))
-        self.panels.append(TimeSeries(title=title, targets=targets, gridPos=GridPos(h=self.panel_height, w=self.panel_width, x=self.current_x, y=self.current_y)))
+            targets.append(
+                Target(
+                    expr=query_text,
+                    legendFormat=query_label,
+                    refId=refId,
+                    datasource=PROMETHEUS_DATASOURCE_NAME,
+                )
+            )
+        self.panels.append(
+            TimeSeries(
+                title=title,
+                targets=targets,
+                gridPos=GridPos(
+                    h=self.panel_height,
+                    w=self.panel_width,
+                    x=self.current_x,
+                    y=self.current_y,
+                ),
+            )
+        )
         self.current_x += self.panel_width
-        if self.current_x >= self.MAX_WIDTH/2:
+        if self.current_x >= self.MAX_WIDTH / 2:
             self.current_y += self.panel_height
             self.current_x = 0
 
     def Render(self):
         return Dashboard(
-            title=self.title,
-            rows=self.rows,
-            panels=self.panels 
+            title=self.title, rows=self.rows, panels=self.panels
         ).auto_panel_ids()
