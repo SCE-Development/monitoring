31e0a0193620b332c1c5fc8e5bbfbe7c65d7eea5
Sujan30 <Nandikolsujan@gmail.com>
2025-07-03 12:49:38 -0700

    used wrapper methods for cleezy transition

--- grafana/provisioning/dashboards/cleezy.dashboard.py
+++ grafana/provisioning/dashboards/cleezy.dashboard.py
@@ -1,97 +1,72 @@
 from grafanalib.core import Dashboard, Templating, Template, TimeSeries, Target, GridPos
 from grafanalib.formatunits import PERCENT_UNIT, SECONDS, NUMBER_FORMAT, TRUE_FALSE
 
+from wrapper import SceGrafanalibWrapper, ExpressionAndLegendPair
 from common import PROMETHEUS_DATASOURCE_NAME
 
 
-dashboard = Dashboard(
-    title='Cleezy',
-    uid='leezy',
-    description='sce club website',
-    timezone='browser',
-    panels=[
-        TimeSeries(
-            title='Cache Size (entries)',
-            unit=NUMBER_FORMAT,
-            gridPos=GridPos(h=8, w=12, x=0, y=0),
-            lineWidth=2,
-            stacking={'group': 'A','mode': 'none'},
-            tooltipMode='all',
-            tooltipSort='desc',
-            targets=[
-                Target(
-                    datasource=PROMETHEUS_DATASOURCE_NAME,
-                    expr='cache_size',
-                    refId='A',
-                ),
-            ],
-        ),
-        TimeSeries(
-            title='SQLite latency by query',
-            unit=NUMBER_FORMAT,
-            gridPos=GridPos(h=8, w=12, x=0, y=0),
-            lineWidth=2,
-            stacking={'group': 'A','mode': 'none'},
-            tooltipMode='all',
-            tooltipSort='desc',
-            targets=[
-                Target(
-                    datasource=PROMETHEUS_DATASOURCE_NAME,
-                    legendFormat="{{query_type}}",
-                    expr='query_time_sum / query_time_count',
-                    refId='A',
-                ),
-            ],
-        ),
-        TimeSeries(
-            title='SQLite latency by query',
-            unit=NUMBER_FORMAT,
-            gridPos=GridPos(h=8, w=12, x=0, y=0),
-            lineWidth=2,
-            stacking={'group': 'A','mode': 'none'},
-            tooltipMode='all',
-            tooltipSort='desc',
-            targets=[
-                Target(
-                    datasource=PROMETHEUS_DATASOURCE_NAME,
-                    legendFormat="{{code}} {{path}}",
-                    expr='http_code_total{path!="/metrics", job="cleezy"}',
-                    refId='A',
-                ),
-            ],
-        ),
-        TimeSeries(
-            title='Cache hits and misses',
-            unit=NUMBER_FORMAT,
-            gridPos=GridPos(h=8, w=12, x=0, y=0),
-            lineWidth=2,
-            stacking={'group': 'A','mode': 'none'},
-            tooltipMode='all',
-            tooltipSort='desc',
-            targets=[
-                Target(
-                    datasource=PROMETHEUS_DATASOURCE_NAME,
-                    legendFormat="{{__name__}}",
-                    expr='cache_hits_total',
-                    refId='A',
-                ),
-            ],
-        ),
-        TimeSeries(
-            title='Container Uptime',
-            unit=NUMBER_FORMAT,
-            gridPos=GridPos(h=8, w=12, x=0, y=0),
-            lineWidth=2,
-            stacking={'group': 'A','mode': 'none'},
-            tooltipMode='all',
-            tooltipSort='desc',
-            targets=[
-                Target(
-                    datasource=PROMETHEUS_DATASOURCE_NAME,
-                    expr='time() - process_start_time_seconds{job="cleezy"}',
-                    refId='A',
-                ),
-            ],
-        ),
+wrapper = SceGrafanalibWrapper("Cleezy")
+
+wrapper.DefineRow("Cache Metrics")
+wrapper.AddPanel(
+    title="Cache Size (entries)",
+    queries=[
+        ExpressionAndLegendPair(
+            'cache_size',
+            "",
+        )
     ],
-).auto_panel_ids()
+    unit=NUMBER_FORMAT,
+    dydt=True
+)
+
+wrapper.AddPanel(
+    title="SQLite latency by query",
+    queries=[
+        ExpressionAndLegendPair(
+            'query_time_sum / query_time_count',
+            "{{query_type}}",
+        )
+    ],
+    unit=NUMBER_FORMAT,
+    dydt=False
+)
+
+# HTTP status codes panel
+wrapper.AddPanel(
+    title="HTTP Status Codes",
+    queries=[
+        ExpressionAndLegendPair(
+            'http_code_total{path!="/metrics", job="cleezy"}',
+            "{{code}} {{path}}",
+        )
+    ],
+    unit=NUMBER_FORMAT,
+    dydt=True
+)
+
+wrapper.AddPanel(
+    title="Cache hits and misses",
+    queries=[
+        ExpressionAndLegendPair(
+            'cache_hits_total',
+            "{{__name__}}",
+        )
+    ],
+    unit=NUMBER_FORMAT,
+    dydt=True
+)
+
+wrapper.AddPanel(
+    title="Container Uptime",
+    queries=[
+        ExpressionAndLegendPair(
+            'time() - process_start_time_seconds{job="cleezy"}',
+            "",
+        )
+    ],
+    unit=NUMBER_FORMAT,
+    dydt=False
+)
+
+dashboard = wrapper.Render()
