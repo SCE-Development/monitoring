c8b7ae1d56e4d671cf43e0623db442b43aa7d1dc
tofu <isabelleluu0w0@gmail.com>
2025-07-05 22:58:40 -0700

    lint fixes

--- grafana/provisioning/dashboards/nodefull.dashboard.py
+++ grafana/provisioning/dashboards/nodefull.dashboard.py
@@ -6,21 +6,32 @@ wrapper = SceGrafanalibWrapper(
     title="Node Exporter Full (wrapper)", panel_height=4, panel_width=3
 )
 wrapper.DefineRow("Quick CPU / Mem / Disk")
-wrapper.DefineTemplating("datasource", "Datasource", "prometheus", 'datasource')
+wrapper.DefineTemplating(
+    "datasource",
+    "Datasource",
+    "prometheus",
+    'datasource'
+)
 wrapper.DefineTemplating("job", "Job", "label_values(node_uname_info, job)")
 wrapper.DefineTemplating(
-    "nodename", "Nodename", 'label_values(node_uname_info{job="$job"}, nodename)'
+    "nodename",
+    "Nodename",
+    'label_values(node_uname_info{job="$job"}, nodename)'
 )
 wrapper.DefineTemplating(
     "node",
     "Instance",
-    'label_values(node_uname_info{job="$job", nodename="$nodename"}, instance)',
+    'label_values(node_uname_info{job="$job", nodename="$nodename"}, instance)'
 )
 wrapper.AddPanel(
     title="CPU Busy",
     queries=[
         ExpressionAndLegendPair(
-            '(1 - avg(rate(node_cpu_seconds_total{mode="idle", instance="$node"}[$__rate_interval])))'
+            '(1 - avg(rate(' +
+            'node_cpu_seconds_total{' +
+            'mode="idle", instance="$node"}' +
+            '[$__rate_interval]' +
+            ')))'
         )
     ],
     unit=PERCENT_UNIT,
@@ -30,7 +41,11 @@ wrapper.AddPanel(
     title="Sys Load",
     queries=[
         ExpressionAndLegendPair(
-            'scalar(node_load1{instance="$node",job="$job"}) / count(count(node_cpu_seconds_total{instance="$node",job="$job"}) by (cpu))'
+            'scalar(node_load1{' +
+            'instance="$node",job="$job"}) / ' +
+            'count(count(node_cpu_seconds_total{' +
+            'instance="$node",job="$job"}) ' +
+            'by (cpu))'
         )
     ],
     unit=PERCENT_UNIT,
@@ -40,7 +55,9 @@ wrapper.AddPanel(
     title="RAM Used",
     queries=[
         ExpressionAndLegendPair(
-            '(1 - (node_memory_MemAvailable_bytes{instance="$node", job="$job"} / node_memory_MemTotal_bytes{instance="$node", job="$job"}))'
+            '(1 - (node_memory_MemAvailable_bytes{' +
+            'instance="$node", job="$job"} / node_memory_MemTotal_bytes{' +
+            'instance="$node", job="$job"}))'
         )
     ],
     unit=PERCENT_UNIT,
@@ -50,7 +67,9 @@ wrapper.AddPanel(
     title="SWAP Used",
     queries=[
         ExpressionAndLegendPair(
-            '((node_memory_SwapTotal_bytes{instance="$node",job="$job"} - node_memory_SwapFree_bytes{instance="$node",job="$job"}) / (node_memory_SwapTotal_bytes{instance="$node",job="$job"}))'
+            '((node_memory_SwapTotal_bytes{instance="$node",job="$job"} - ' +
+            'node_memory_SwapFree_bytes{instance="$node",job="$job"}) / ' +
+            '(node_memory_SwapTotal_bytes{instance="$node",job="$job"}))'
         )
     ],
     unit=PERCENT_UNIT,
@@ -60,7 +79,15 @@ wrapper.AddPanel(
     title="Root FS Used",
     queries=[
         ExpressionAndLegendPair(
-            '(\n  (node_filesystem_size_bytes{instance="$node", job="$job", mountpoint="/", fstype!="rootfs"}\n   - node_filesystem_avail_bytes{instance="$node", job="$job", mountpoint="/", fstype!="rootfs"})\n  / node_filesystem_size_bytes{instance="$node", job="$job", mountpoint="/", fstype!="rootfs"}\n)\n'
+            '(\n  (node_filesystem_size_bytes{' +
+            'instance="$node", job="$job", mountpoint="/", fstype!="rootfs"' +
+            '}\n   - ' +
+            'node_filesystem_avail_bytes{' +
+            'instance="$node", job="$job", mountpoint="/", fstype!="rootfs"' +
+            '})\n  / ' +
+            'node_filesystem_size_bytes{' +
+            'instance="$node", job="$job", mountpoint="/", fstype!="rootfs"' +
+            '}\n)\n'
         )
     ],
     unit=PERCENT_UNIT,
