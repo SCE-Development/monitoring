e78391e06341739792a698315db8819b759a67c1
Samson Xu <110848218+xusamson8@users.noreply.github.com>
2025-07-15 23:34:33 -0700

    Update Wrapper, and use Wrapper for Cadvisor (#48)
    * update wrapper to support other visuals, and have dropdown for templating

--- grafana/provisioning/dashboards/cadvisor.dashboard.py
+++ grafana/provisioning/dashboards/cadvisor.dashboard.py
@@ -5,97 +5,66 @@ from grafanalib.core import (
 )
 from grafanalib.formatunits import BYTES_IEC, SECONDS, BYTES_SEC_IEC
 
-from common import PROMETHEUS_DATASOURCE_NAME
+from wrapper import SceGrafanalibWrapper, ExpressionAndLegendPair
 
+wrapper = SceGrafanalibWrapper(title='Cadvisor')
 
-dashboard = Dashboard(
-    title='Cadvisor',
-    uid='cadvisor',
-    description='Container metrics collected by Cadvisor',
-    tags=[
-        'linux',
-        'docker',
+wrapper.DefineTemplating(
+    label='Compose Project',
+    query='label_values({__name__=~"container.*"}, container_label_com_docker_compose_project)',
+)
+
+wrapper.DefineTemplating(
+    label='Container',
+    query='label_values({__name__=~"container.*", container_label_com_docker_compose_project=~"$compose_project"}, name)',
+)
+
+wrapper.AddPanel(
+    title="Container Memory Usage",
+    queries=[
+        ExpressionAndLegendPair(
+            'max by (name) (container_memory_usage_bytes{name=~"$container", container_label_com_docker_compose_project=~"$compose_project"})',
+            '{{ name }}',
+        )
     ],
-    templating=Templating(list=[
-        Template(
-            name='compose_project',
-            label='Compose Project',
-            dataSource=PROMETHEUS_DATASOURCE_NAME,
-            query='label_values({__name__=~"container.*"}, container_label_com_docker_compose_project)',
-            includeAll=True,
-            multi=True,
-            refresh=REFRESH_ON_TIME_RANGE_CHANGE,
-        ),
-        Template(
-            name='container_name',
-            label='Container',
-            dataSource=PROMETHEUS_DATASOURCE_NAME,
-            query='label_values({__name__=~"container.*", container_label_com_docker_compose_project=~"$compose_project"}, name)',
-            includeAll=True,
-            multi=True,
-            refresh=REFRESH_ON_TIME_RANGE_CHANGE,
-        ),
-    ]),
-    timezone='browser',
-    panels=[
-        TimeSeries(
-            title='Container Memory Usage',
-            unit=BYTES_IEC,
-            gridPos=GridPos(h=8, w=12, x=0, y=0),
-            lineWidth=2,
-            fillOpacity=10,
-            showPoints='never',
-            stacking={'mode': 'normal'},
-            tooltipMode='all',
-            tooltipSort='desc',
-            targets=[
-                Target(
-                    datasource=PROMETHEUS_DATASOURCE_NAME,
-                    expr='max by (name) (container_memory_usage_bytes{name=~"$container_name", container_label_com_docker_compose_project=~"$compose_project"})',
-                    legendFormat='{{ name }}',
-                    refId='A',
-                ),
-            ],
-        ),
-        TimeSeries(
-            title='Container CPU Usage',
-            unit=SECONDS,
-            gridPos=GridPos(h=8, w=12, x=12, y=0),
-            lineWidth=2,
-            fillOpacity=10,
-            showPoints='never',
-            targets=[
-                Target(
-                    datasource=PROMETHEUS_DATASOURCE_NAME,
-                    expr='max by (name) (rate(container_cpu_usage_seconds_total{name=~"$container_name", container_label_com_docker_compose_project=~"$compose_project"}[$__rate_interval]))',
-                    legendFormat='{{ name }}',
-                    refId='A',
-                ),
-            ],
-        ),
-        TimeSeries(
-            title='Container Network Traffic',
-            unit=BYTES_SEC_IEC,
-            gridPos=GridPos(h=8, w=12, x=0, y=8),
-            lineWidth=2,
-            fillOpacity=10,
-            showPoints='never',
-            tooltipMode='all',
-            tooltipSort='desc',
-            targets=[
-                Target(
-                    datasource=PROMETHEUS_DATASOURCE_NAME,
-                    expr='max by (name) (rate(container_network_receive_bytes_total{name=~"$container_name", container_label_com_docker_compose_project=~"$compose_project"}[$__rate_interval]))',
-                    legendFormat="rx {{ name }}",
-                    refId='A',
-                ),
-                Target(
-                    datasource=PROMETHEUS_DATASOURCE_NAME,
-                    expr='-max by (name) (rate(container_network_transmit_bytes_total{name=~"$container_name", container_label_com_docker_compose_project=~"$compose_project"}[$__rate_interval]))',
-                    legendFormat="tx {{ name }}",
-                    refId='B',
-                ),
-            ],
-        ),
+    unit=BYTES_IEC,
+    lineWidth=2,
+    fillOpacity=10,
+    showPoints='never',
+    stacking={'mode': 'normal'},
+)
+
+wrapper.AddPanel(
+    title="Container CPU Usage",
+    queries=[
+        ExpressionAndLegendPair(
+            'max by (name) (rate(container_cpu_usage_seconds_total{name=~"$container", container_label_com_docker_compose_project=~"$compose_project"}[$__rate_interval]))',
+            '{{ name }}',
+        )
     ],
-).auto_panel_ids()
+    unit=SECONDS,
+    lineWidth=2,
+    fillOpacity=10,
+    showPoints='never',
+)
+
+wrapper.AddPanel(
+    title="Container Network Traffic",
+    queries=[
+        ExpressionAndLegendPair(
+            'max by (name) (rate(container_network_receive_bytes_total{name=~"$container", container_label_com_docker_compose_project=~"$compose_project"}[$__rate_interval]))',
+            'rx {{ name }}',
+        ),
+        ExpressionAndLegendPair(
+            '-max by (name) (rate(container_network_transmit_bytes_total{name=~"$container", container_label_com_docker_compose_project=~"$compose_project"}[$__rate_interval]))',
+            'tx {{ name }}',
+        )
+    ],
+    unit=BYTES_SEC_IEC,
+    lineWidth=2,
+    fillOpacity=10,
+    showPoints='never',
+    stacking={'mode': 'normal'},
+)
+
+dashboard = wrapper.Render()
