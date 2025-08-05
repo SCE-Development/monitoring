32d474ad37f2298950f69c862da4385a1915d762
durpdur <durpdur@gmail.com>
2025-08-05 14:08:01 -0700

    Added dashboard for current epoch time, refresh token expired

--- grafana/provisioning/dashboards/email.dashboard.py
+++ grafana/provisioning/dashboards/email.dashboard.py
@@ -3,6 +3,28 @@ from grafanalib.formatunits import NUMBER_FORMAT
 
 wrapper = SceGrafanalibWrapper("Email Tracking")
 
+wrapper.DefineRow("Current Time")
+wrapper.AddPanel(
+    title="Current Epoch Time",
+    queries=[
+        ExpressionAndLegendPair(
+            'current_epoch_time{app="sce-core"}',
+            'Current Epoch Time'
+        )
+    ],
+)
+
+wrapper.DefineRow("Refresh Token Expired")
+wrapper.AddPanel(
+    title="Refresh Token Refreshed",
+    queries=[
+        ExpressionAndLegendPair(
+            'google_cloud_refresh_token_epoch{app="sce-core"}',
+            'Refresh token expired and refreshed'
+        )
+    ],
+)
+
 wrapper.DefineRow("Email Sent")
 wrapper.AddPanel(
     title="Total Emails Sent",
