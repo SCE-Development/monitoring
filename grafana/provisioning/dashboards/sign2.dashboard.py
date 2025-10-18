a91e99d8bf1ef9121f4568926e5578fd59021d65
adarsh <110150037+adarshm11@users.noreply.github.com>
2025-10-17 23:22:01 -0700

    create dashboard for sign2 (#81)
    * dashboard for sign2
    
    * add uptime metric
    
    * misspelled container bruh
    
    * remove /metrics from endpoint hits + specify time units
    
    * fix all dashboards for sign2
    
    * use regex to omit endpoints
    
    * amend regex parsing
    
    * add job name to all metrics lolz

---
+++ grafana/provisioning/dashboards/sign2.dashboard.py
@@ -0,0 +1,75 @@
+from grafanalib.core import Dashboard, Templating, Template, TimeSeries, Target, GridPos
+from grafanalib.formatunits import SECONDS
+
+from wrapper import SceGrafanalibWrapper, ExpressionAndLegendPair
+
+wrapper = SceGrafanalibWrapper(title='sign2')
+
+wrapper.AddPanel(
+    title="Sign last updated",
+    queries=[
+        ExpressionAndLegendPair(
+            'time() - sign_last_updated{job="sce-sign2"}',
+        ),
+    ],
+    unit=SECONDS
+)
+
+wrapper.AddPanel(
+    title="Sign update error",
+    queries=[
+        ExpressionAndLegendPair(
+            'sign_update_error{job="sce-sign2"}'
+        )
+ ],
+)
+
+wrapper.AddPanel(
+    title="LeetCode API Error",
+    queries=[
+        ExpressionAndLegendPair(
+            'leetcode_api_error{job="sce-sign2"}'
+        )
+ ],
+)
+
+wrapper.AddPanel(
+    title="Endpoint hits",
+    queries=[
+        ExpressionAndLegendPair(
+            'endpoint_hits_total{job="sce-sign2", path!~"/metrics|.*[.].*"}',
+            "{{code}} {{path}}",
+        )
+    ],
+)
+
+wrapper.AddPanel(
+    title="LeetCode API Response Codes",
+    queries=[
+        ExpressionAndLegendPair(
+            'leetcode_api_response_codes_total{job="sce-sign2"}',
+            "{{code}}",
+        )
+    ],
+)
+
+wrapper.AddPanel(
+    title="LeetCode API latency",
+    queries=[
+        ExpressionAndLegendPair(
+            'leetcode_api_latency_sum / leetcode_api_latency_count{job="sce-sign2"}',
+        )
+    ],
+)
+
+wrapper.AddPanel(
+    title="Container Uptime",
+    queries=[
+        ExpressionAndLegendPair(
+            'time() - process_start_time_seconds{job="sce-sign2"}',
+        )
+    ],
+    unit=SECONDS
+)
+
+dashboard = wrapper.Render()
