6c781c9777b08b8341874417b238f6ae5e3a0fcc
adarshm11 <amlearning2023@gmail.com>
2026-03-12 12:34:01 -0700

    goderpad dashboard not tested yet

---
+++ grafana/provisioning/dashboards/goderpad.dashboard.py
@@ -0,0 +1,103 @@
+from grafanalib.core import Dashboard, Templating, Template, TimeSeries, Target, GridPos
+from grafanalib.formatunits import SECONDS
+
+from wrapper import SceGrafanalibWrapper, ExpressionAndLegendPair
+
+wrapper = SceGrafanalibWrapper(title='goderpad')
+
+
+wrapper.AddPanel(
+    title="Container uptime",
+    queries=[
+        ExpressionAndLegendPair(
+            'time() - process_start_time_seconds{job="goderpad"}',
+        ),
+    ],
+    unit=SECONDS,
+)
+
+wrapper.AddPanel(
+    title="Endpoint Hits",
+    queries=[
+        ExpressionAndLegendPair(
+            'endpoint_hits_total{job="goderpad", path!~"/metrics|.*[.].*"}',
+            "{{code}} {{path}}",
+        ),
+    ],
+)
+
+wrapper.AddPanel(
+    title="HTTP Request Duration Seconds",
+    queries=[
+        ExpressionAndLegendPair(
+            'http_request_duration_seconds{job="goderpad"}',
+            "{{code}} {{path}}",
+        ),
+    ],
+    unit=SECONDS,
+)
+
+wrapper.AddPanel(
+    title="Rooms Active",
+    queries=[
+        ExpressionAndLegendPair(
+            'rooms_active{job="goderpad"}',
+        ),
+    ],
+)
+
+wrapper.AddPanel(
+    title="Room Users Total",
+    queries=[
+        ExpressionAndLegendPair(
+            'room_users_total{job="goderpad"}',
+        ),
+    ],
+)
+
+wrapper.AddPanel(
+    title="Room Join Errors Total",
+    queries=[
+        ExpressionAndLegendPair(
+            'room_join_errors_total{job="goderpad"}',
+        ),
+    ],
+)
+
+wrapper.AddPanel(
+    title="Room Create Errors Total",
+    queries=[
+        ExpressionAndLegendPair(
+            'room_create_errors_total{job="goderpad"}',
+        ),
+    ],
+)
+
+wrapper.AddPanel(
+    title="WebSocket Upgrade Errors Total",
+    queries=[
+        ExpressionAndLegendPair(
+            'websocket_upgrade_errors_total{job="goderpad"}',
+        ),
+    ],
+)
+
+wrapper.AddPanel(
+    title="Document Saves Errors Total",
+    queries=[
+        ExpressionAndLegendPair(
+            'document_saves_errors_total{job="goderpad"}',
+        ),
+    ],
+)
+
+wrapper.AddPanel(
+    title="Room Expiry Last Run",
+    queries=[
+        ExpressionAndLegendPair(
+            'room_expiry_last_run{job="goderpad"}',
+        ),
+    ],
+)
+
+dashboard = wrapper.Render()
